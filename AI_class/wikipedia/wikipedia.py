ec515376c626abbc2378c2cb446eb33160073a1c
lb9849 <lb9849@rit.edu>
2021-05-02 22:13:34 -0400

    Working fully for decision tree

--- AI_class/wikipedia/wikipedia.py
+++ AI_class/wikipedia/wikipedia.py
@@ -13,15 +13,21 @@ def load_data(file):  # todo fix to vanilla python
         data.append(row)
     return data
 
-def prepare_data(data):
-    """Prepare data for training and prediction : expands the features, delete the text, returns colnmaes, label index and text"""
+def prepare_data(data, type = 'train'):
+    """Prepare data for training and prediction : expands the features, delete the text, returns colnames, label index and text"""
     features = feature_expansion(data)
     text = select_column(data, 0)
     for x in data:
         del x[0]
-    colnames = ['label'] + features #  FIX this !!!
-   # colnames = features #  FIX this !!!
-    label_index = colnames.index('label')
+    if type == 'train':
+        colnames = ['label'] + features #  FIX this !!!
+        label_index = 0
+    else:
+        colnames = features
+        label_index = None
+    #colnames = features #  FIX this !!!
+    # label_index = colnames.index('label')
+
     return colnames, label_index, text
 
 
@@ -155,21 +161,30 @@ def dtree_build(data, list_attr, label_index, eps = 10 ** -4):
         rnode.add_child(subtree, answer)
     return rnode
 
-def expand(data, type):
-    if type != "vowel_ending":
-        for obs in data:
-            feature = obs[0].count(type) > 0
-            obs.append(feature)
-    elif type == 'vowel_ending':
-        pass
 
+def count_end_vowels(list_words):
+    count = 0
+    for word in list_words:
+        if word[-1] in ['i', 'a', 'e', 'è', 'o', 'é', 'ŭ', 'ò', 'ù']:
+            count += 1
+    return count
+
+def expand(data, type):
+    for obs in data:
+        if type == "vowel_ending":
+            splitted = obs[0].split() #split to list of words
+            feature = count_end_vowels(splitted) >= 10
+        else:
+            feature = obs[0].count(type) > 0
+        obs.append(feature)
 
 def feature_expansion(data):
     """Returns the original data with additional feature columns for language identification
     :param data: list of lists in format [[Text_i, label_i]]
-    :return data_exp: expanded data in formate [[Text_i, label_i, feature1_i ... featurek_i]
+    :return data_exp: expanded data in format [[Text_i, label_i, feature1_i ... featurek_i]
     """
-    feature_list = ['oo', 'ooi', 'aa', 'ee', 'aai', 'auw', 'eu', 'euw', 'uu', 'è', 'é', 'ei', '\'', 'ŭ', 'ò', 'ù', 'vv']  # future attribute names
+    #feature_list = ['vowel_ending', 'ooi', 'aa', 'ee', 'aai', 'auw', 'eu', 'euw', 'uu', 'è', 'é', 'ei', '\'', 'ŭ', 'ò', 'ù', 'vv']  # future attribute names
+    feature_list = ['vowel_ending',  'aa', 'ee', 'uu', 'è', 'ei', '\'', 'ò', 'vv'] # FIX: remove uu, vv?
     for f in feature_list:
         expand(data, f)
     return feature_list
@@ -232,13 +247,19 @@ def testing():
     # b_attr, answers, attr_ind = best_attribute(data, colnames, label_index)
     # list_attr = [x for i, x in enumerate(colnames) if i != label_index]  # create a list of attribute names
 
-
     print("Test dataset")
     test_data = load_data("wikipedia_data_test.csv")
     colnames, label_index, text = prepare_data(test_data)
     results = predict_all(test_data, tree, colnames)
     acc = accuracy(test_data, label_index, results)
     print(acc)
+
+    print("#### TEST CONSISTENCY WITH SPECS")
+    r_s_data = load_data("data_right_spec.txt")
+    colnames, _, _ = prepare_data(r_s_data, 'predict')
+    predicted_labels = predict_all(r_s_data, tree, colnames)
+    print(predicted_labels)
+
 
 def infer_tree():
     train_data = load_data("wikipedia_data_train.csv")
@@ -248,29 +269,59 @@ def infer_tree():
     best_eps = None
     best_acc = 0
     for eps in range(5, 95, 5):
-        print(eps)
         tree = dtree_build(train_data, colnames, label_index, eps/100)
         predicted_labels = predict_all(valid_data, tree, colnames)
         acc = accuracy(valid_data, label_index, predicted_labels)
-        print(eps, acc)
         if acc > best_acc:
             best_acc = acc
             best_eps = eps
-    print("Best entropy threshold", best_eps/100, " with accuracy ", best_acc)
+    print("Best entropy threshold on the validation set is", best_eps/100, " with accuracy ", best_acc)
+    print("But the second best entropy threshold is chosen (see report)", 0.3, " with accuracy ", '0.958')
 
-    tree = dtree_build(train_data, colnames, label_index, 0.3)
+    tree = dtree_build(train_data, colnames, label_index, 0.4)
     test_data = load_data("wikipedia_data_test.csv")
     _, _, _ = prepare_data(test_data)
     predicted_labels = predict_all(test_data, tree, colnames)
     acc = accuracy(test_data, label_index, predicted_labels)
+    print("Accuracy of the best tree with eps = 0.3 on test data: ", acc)
 
-    r_s_data = load_data("data_right_spec.txt")
-    colnames, _, _ = prepare_data(r_s_data)
-    #predicted_labels = predict_all(r_s_data, tree, colnames)
-    #acc = accuracy(test_data, label_index, predicted_labels)
+def make_best_tree():
+    #  feature_list = ['vowel_ending',  'aa', 'ee', 'uu', 'è', 'ei', '\'', 'ò', 'vv']
+    node1 = Node('uu', False, children = [Node('nl', answer=False), Node('nl', answer=True)])
+    node2 = Node('ò', True, children = [Node('it', answer=False), Node('it', answer=True)])
+    node3 = Node('\'', False, children=[node1, node2])
+    node4 = Node('\'', True, children=[Node('nl', answer=False), Node('nl', answer=True)])
+    node5 = Node('ei', False, children=[node3, node4])
+    node6 = Node('è', False, children=[node5, Node('it', answer = True)])
+    node7 = Node('ee', False, children=[node6, Node('nl', True)])
+    node8 = Node('aa', False, children=[node7, Node('nl', True)])
+    root = Node('vowel_ending', children=[node8, Node('it', True)])
+    return root
+
 if __name__ == '__main__':
+    mode = sys.argv[1]
 
-    infer_tree()
+    if mode == "predict":
+        type = sys.argv[2]
+        filename = sys.argv[3]
+
+    if mode == 'train':
+        infer_tree()
+
+    elif mode == 'predict':
+        data = load_data(filename)
+        tree = make_best_tree()
+        colnames, _, _ = prepare_data(data, 'predict')
+        #colnames, label_index, text = prepare_data(data, 'predict') #fiX!
+        predicted_labels = predict_all(data, tree, colnames)
+        #acc = accuracy(data, label_index, predicted_labels)
+        for label in predicted_labels:
+            if label == 'it':
+                print('Italian')
+            elif label =='nl':
+                print('Dutch')
+    else:
+        raise ValueError("Unsupported argument. Correct usage: python3 wiki.py [train/predict] [model-type] [datafile] (last two arguments only for predict mode)")
 
 # def best_attribute(, A)
 # colnames = ['text', 'label']
